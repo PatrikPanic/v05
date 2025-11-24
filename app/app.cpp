62187a5085de30403e1445f13635e20a98115d86
PatrikPanic <papanic@vsite.hr>
2025-11-24 20:44:39 +0100

    V05 solution p1

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,47 @@
 
 namespace vsite::oop::v5
 {
+        double operator"" _inch(unsigned long long v)
+        {
+            return v * 2.54;
+        }
 
-}
+        double operator"" _eur(unsigned long long v)
+        {
+            return v * 7.5345;
+        }
+
+        tire::tire(double diameter) : d(diameter) {}
+
+        double tire::diameter() const
+        {
+            return d;
+        }
+
+
+        vehicle::vehicle(int year) : y(year) {}
+
+        int vehicle::year() const
+        {
+            return y;
+        }
+
+       
+        product::product(double price) : p(price) {}
+
+        double product::price() const
+        {
+            return p;
+        }
+
+        
+        car::car(int year, double price, double tire_diam)
+            : vehicle(year), product(price), t(tire_diam)
+        {
+        }
+
+        double car::tire_diameter() const
+        {
+            return t.diameter();
+        }
+    }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,36 @@
 
 namespace vsite::oop::v5 
 {
+        double operator"" _inch(unsigned long long v);
+        double operator"" _eur(unsigned long long v);
 
+        class tire {
+            double d;
+        public:
+            explicit tire(double diameter);
+            double diameter() const;
+        };
+
+        class vehicle {
+            int y;
+        public:
+            explicit vehicle(int year);
+            int year() const;
+        };
+
+        class product {
+            double p;
+        public:
+            explicit product(double price);
+            double price() const;
+        };
+
+        class car : public vehicle, public product
+        {
+            tire t;
+        public:
+            car(int year, double price, double tire_diam);
+
+            double tire_diameter() const;
+        };
 }
